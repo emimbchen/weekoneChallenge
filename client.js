84695b2a73ea6883f2a407e7c2919835bc753a17
Emi Chen <emimbchen@gmail.com>
2017-10-06 16:59:42 -0500

    actually fixed loop

--- client.js
+++ client.js
@@ -7,11 +7,13 @@ function queryGo(){
 $('button').on('click', buttonClick);
 }
 var inputValue;
+var $inputArray;
+
 function buttonClick(){
-var trow = $('#tableDiv').append('<tr></tr>');
-var $inputArray = $('#inputDiv').children();
-for(var i = 0; i < ($inputArray).length; i++){
-var inputValue = $($inputArray[i]).val();
-$(trow).append('<td>' + inputValue + '</td>');
-}
+ var trow = $('table').append('<tr></tr>');
+ var $inputArray = $('#inputDiv').children();
+ for(var i = 0; i < ($inputArray).length; i++){
+ var inputValue = $($inputArray[i]).val();
+ $(trow).append('<td>' + inputValue + '</td>');
+ }
 }
