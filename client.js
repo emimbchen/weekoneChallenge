a4ef154be277c191f559ad5757241c6712fa78c5
Emi Chen <emimbchen@gmail.com>
2017-10-06 16:32:22 -0500

    completed val loop

--- client.js
+++ client.js
@@ -4,5 +4,14 @@ $(queryGo);
 
 function queryGo(){
   console.log('query went');
-
+$('button').on('click', buttonClick);
+}
+var inputValue;
+function buttonClick(){
+var trow = $('#tableDiv').append('<tr></tr>');
+var $inputArray = $('#inputDiv').children();
+for(var i = 0; i < ($inputArray).length; i++){
+var inputValue = $($inputArray[i]).val();
+$(trow).append('<td>' + inputValue + '</td>');
+}
 }
