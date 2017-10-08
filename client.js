291ececa415f61a76e8be7029b388ebc02d44810
Emi Chen <emimbchen@gmail.com>
2017-10-08 18:38:49 -0500

    ok now I fixed indents

--- client.js
+++ client.js
@@ -4,43 +4,43 @@ $(queryGo);
 
 function queryGo(){
   console.log('query went');
-$('button').on('click', buttonClick);
-$('table').on('click', 'button', removeButton);
+  $('button').on('click', buttonClick);
+  $('table').on('click', 'button', removeButton);
 }
 
 var totalCosts;
 //when button is clicked add all inputs to table
 function buttonClick(){
-//resets rowData per click
-var rowData;
-//creates row via "for" loop
- var $inputArray = $('#inputDiv').children();
- for(var i = 0; i < ($inputArray).length - 1; i++){
- var inputValue = $($inputArray[i]).val();
- rowData+= "<td>" + inputValue + "</td>";
- }
- //append row after array is complete
- //adds the remove button to row
- var trow = $('table').append('<tr>' + rowData + '<td>' + '<button>' + 'Remove' +'</button>'+ '</td>' + '</tr>');
+  //resets rowData per click
+  var rowData;
+  //creates row via "for" loop
+  var $inputArray = $('#inputDiv').children();
+  for(var i = 0; i < ($inputArray).length - 1; i++){
+    var inputValue = $($inputArray[i]).val();
+    rowData+= "<td>" + inputValue + "</td>";
+  }
+  //append row after array is complete
+  //adds the remove button to row
+  var trow = $('table').append('<tr>' + rowData + '<td>' + '<button>' + 'Remove' +'</button>'+ '</td>' + '</tr>');
 
-//add salary to $('span')
- var salary = parseInt($($inputArray[4]).val());
+  //add salary to $('span')
+  var salary = parseInt($($inputArray[4]).val());
 
- //clear input values
-$('input').val('');
+  //clear input values
+  $('input').val('');
 
-//calculate the costs
-totalCosts = calculateCosts(salary);
-$('span').text(totalCosts);
-return totalCosts;
+  //calculate the costs
+  totalCosts = calculateCosts(salary);
+  $('span').text(totalCosts);
+  return totalCosts;
 }
 
 //function for the math
 function calculateCosts(salary){
-var $currentTotal = parseInt($('span').text());
-salary= Math.round(salary/12);
-$currentTotal += salary;
-return $currentTotal;
+  var $currentTotal = parseInt($('span').text());
+  salary= Math.round(salary/12);
+  $currentTotal += salary;
+  return $currentTotal;
 }
 
 function removeButton(){
